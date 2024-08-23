69ad5f8199815184dd5fe6c9058f99cace33afe8
Cmattr <cmattr@outlook.com>
2024-08-23 15:23:15 -0400

    testing

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -23,6 +23,7 @@ const AddProduct = () => {
     const queryClient = useQueryClient();
     const [showSuccessAlert, setShowSucessAlert] = useState(false);
 
+    
     const { mutate, isLoading, isError, error} = useMutation({
         mutationFn: postProduct,
         onSuccess: (data) => {
